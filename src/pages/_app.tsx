2b2e5d1253a853d67210a356c3a9212b42cbf9a7
Dimas Wibowo <daemswibowo@gmail.com>
2023-12-14 08:39:57 +0700

    setup google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,19 +7,37 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 import { AnimatePresence } from "framer-motion";
 import { useRouter } from "next/router";
 import NavigationHeader from "@/components/organisms/home/navigationHeader";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
   return (
-    <ChakraProvider theme={theme}>
-      <AnimatePresence mode={"wait"} initial={true}>
-        <BaseProvider menus={pageProps.menus}>
-          <NavigationHeader />
-          <Component key={router.asPath} {...pageProps} />
-        </BaseProvider>
-      </AnimatePresence>
-      <SpeedInsights />
-    </ChakraProvider>
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
+      />
+
+      <Script id="google-analytics" strategy="lazyOnload">
+        {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+      </Script>
+      <ChakraProvider theme={theme}>
+        <AnimatePresence mode={"wait"} initial={true}>
+          <BaseProvider menus={pageProps.menus}>
+            <NavigationHeader />
+            <Component key={router.asPath} {...pageProps} />
+          </BaseProvider>
+        </AnimatePresence>
+        <SpeedInsights />
+      </ChakraProvider>
+    </>
   );
 }
