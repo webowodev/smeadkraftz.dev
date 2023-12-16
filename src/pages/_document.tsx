9f23306274bd2993e217824bf908bf7598fa7963
Dimas Wibowo <daemswibowo@gmail.com>
2023-12-16 14:17:42 +0700

    Add meticulous E2E AI

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,17 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {(process.env.NODE_ENV === "development" ||
+          process.env.VERCEL_ENV === "preview") && (
+          // eslint-disable-next-line @next/next/no-sync-scripts
+          <script
+            data-project-id="UtSuUsNdn7ThhMjRlL7rlHTJT7HTJMpS2iQANUht"
+            data-is-production-environment="false"
+            src="https://snippet.meticulous.ai/v1/meticulous.js"
+          />
+        )}
+      </Head>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <body>
         <Main />
