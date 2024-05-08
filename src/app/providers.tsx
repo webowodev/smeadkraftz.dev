0e3f438f70af2d974d4aec27decd1389dbb889b3
Dimas Wibowo <daemswibowo@gmail.com>
2024-05-08 20:23:56 +0700

    Add lenis for smoothscroll

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -5,6 +5,7 @@ import { IMenus } from "@/lib/entities/menu";
 import BaseProvider from "@/providers/baseProvider";
 import { ChakraProvider } from "@chakra-ui/react";
 import { AnimatePresence } from "framer-motion";
+import ReactLenis from "lenis/react";
 
 export function Providers({
   children,
@@ -14,10 +15,12 @@ export function Providers({
   menus?: IMenus;
 }) {
   return (
-    <ChakraProvider theme={theme}>
-      <AnimatePresence mode={"wait"} initial={true}>
-        <BaseProvider menus={menus}>{children}</BaseProvider>
-      </AnimatePresence>
-    </ChakraProvider>
+    <ReactLenis root>
+      <ChakraProvider theme={theme}>
+        <AnimatePresence mode={"wait"} initial={true}>
+          <BaseProvider menus={menus}>{children}</BaseProvider>
+        </AnimatePresence>
+      </ChakraProvider>
+    </ReactLenis>
   );
 }
